a9ab0978867da32565573a4c7c970b303095eca1
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-11-28 20:23:19 +0100

    Fix

--- SolidCP/Sources/SolidCP.EnterpriseServer.Code/System/HostBillServer.cs
+++ SolidCP/Sources/SolidCP.EnterpriseServer.Code/System/HostBillServer.cs
@@ -11,7 +11,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace SolidCP.EnterpriseServer;
 
